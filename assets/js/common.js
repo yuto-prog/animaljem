b6144d261ae79fdd77a31aa1e7a88080292c40a3
yuto-prog <kidreamer59@gmail.com>
2024-08-09 12:29:00 +0900

    edit

---
+++ assets/js/common.js
@@ -0,0 +1,10 @@
+function copyButton() {
+    // 指定したIDの要素のテキストを取得
+    var element = document.getElementById('address');
+
+    // テキストをクリップボードにコピー
+    navigator.clipboard.writeText(element.textContent);
+    alert
+}
+
+// フラッシュメッセージ　https://qiita.com/ad1117wi/items/9b089436e65564edd3ad
